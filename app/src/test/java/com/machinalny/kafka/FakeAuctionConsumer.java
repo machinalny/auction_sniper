9c7a8a61c393746bc3b6ccd58f9ad19a71822a20
weapecet <weapecet@gmail.com>
2022-08-12 19:02:37 +0200

    Walking Skeleton test pass

--- app/src/test/java/com/machinalny/kafka/FakeAuctionConsumer.java
+++ app/src/test/java/com/machinalny/kafka/FakeAuctionConsumer.java
@@ -1,7 +1,9 @@
 package com.machinalny.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CountDownLatch;
@@ -11,6 +13,10 @@ import java.util.concurrent.TimeUnit;
 public class FakeAuctionConsumer {
 
     private CountDownLatch latch = new CountDownLatch(1);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     private String payload;
     
     @KafkaListener(topics = {"item-54321", "item-65432"}, groupId = "auctionService")
@@ -28,10 +34,12 @@ public class FakeAuctionConsumer {
     }
 
     public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
-        boolean messageConsumed = this.latch.await(10, TimeUnit.SECONDS);
+        boolean messageConsumed = this.latch.await(20, TimeUnit.SECONDS);
         if (!messageConsumed){
             throw new RuntimeException("Didn't got any message");
         }
+
+        kafkaTemplate.sendDefault("LOST" + "item-54321" );
 
     }
 
